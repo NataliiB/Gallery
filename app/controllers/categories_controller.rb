55ff9df524ea3a7ddf59ddee971d036480dae53b
Natalia <YOUR@EMAIL.com>
2024-02-06 11:55:41 +0100

    Added creating category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,29 @@
 class CategoriesController < ApplicationController
-
-    def index
+  def index
       @categories = Category.all
-    end
+  end
 
-    def show
+  def show
       @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    if @category.save
+      redirect_to @category
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 
 end
